b46664dcbb9ccfa68f724e2d27b166a3e34873b1
aiot-anhd <anhd@aiot-global.com>
2025-10-15 15:41:47 +0700

    feature/google-login

--- app/public/wp-content/themes/mooms_dev_v1/resources/scripts/admin/index.js
+++ app/public/wp-content/themes/mooms_dev_v1/resources/scripts/admin/index.js
@@ -1,2 +1,5 @@
 import "@styles/admin";
 import "@scripts/admin/custom_thumbnail_support.js";
+// SweetAlert2 for admin (expose globally for inline hooks)
+import Swal from "sweetalert2";
+window.Swal = Swal;
