2b6e7066128be7259cd6b4030b394b7c8cbfaab9
aiot-anhd <anhd@aiot-global.com>
2025-10-18 22:17:33 +0700

    feature/use-color-in-theme-option

--- app/public/wp-content/themes/mooms_dev_v1/theme/setup/theme-options.php
+++ app/public/wp-content/themes/mooms_dev_v1/theme/setup/theme-options.php
@@ -16,12 +16,21 @@ $optionsPage = Container::make('theme_options', __('MMS Theme', 'mms'))
 	->set_page_file('app-theme-options.php')
 	->set_page_menu_position(3)
 	->add_tab(__('Branding | Thương hiệu', 'mms'), [
+		Field::make('color', 'primary_color', __('Primary Color', 'mms'))
+		->set_width(50)
+		->set_default_value('#010101'),
+		Field::make('color', 'secondary_color', __('Secondary Color', 'mms'))
+		->set_width(50)
+		->set_default_value('#626262'),
+
 		Field::make('image', 'logo', __('Logo', 'mms'))
 			->set_width(33.33),
 		Field::make('image', 'logo_dark', __('Logo Dark', 'mms'))
 			->set_width(33.33),
 		Field::make('image', 'default_image', __('Default image | Hình ảnh mặc định', 'mms'))
 			->set_width(33.33),
+
+
 		Field::make('textarea', 'slogan' . currentLanguage(), __('', 'mms'))
 			->set_attribute('placeholder', 'mooms.dev slogan'),
 	])
@@ -37,16 +46,30 @@ $optionsPage = Container::make('theme_options', __('MMS Theme', 'mms'))
 		Field::make('text', 'phone_number', __('', 'mms'))
 			->set_width(33.33)
 			->set_attribute('placeholder', 'Phone number | Số điện thoại'),
+
 		Field::make('html', 'socials', __('', 'mms'))
 			->set_html('----<i> Socials | Mạng xã hội </i>----'),
-		Field::make('text', 'facebook', __('', 'mms'))->set_width(50)
+		Field::make('image', 'icon_zalo', __('', 'mms'))->set_width(20),
+		Field::make('text', 'zalo', __('', 'mms'))->set_width(30)
+			->set_attribute('placeholder', 'zalo'),
+		Field::make('image', 'icon_facebook', __('', 'mms'))->set_width(20),
+		Field::make('text', 'facebook', __('', 'mms'))->set_width(30)
 			->set_attribute('placeholder', 'facebook'),
-		Field::make('text', 'instagram', __('', 'mms'))->set_width(50)
+		Field::make('image', 'icon_instagram', __('', 'mms'))->set_width(20),
+		Field::make('text', 'instagram', __('', 'mms'))->set_width(30)
 			->set_attribute('placeholder', 'instagram'),
-		Field::make('text', 'tiktok', __('', 'mms'))->set_width(50)
+		Field::make('image', 'icon_tiktok', __('', 'mms'))->set_width(20),
+		Field::make('text', 'tiktok', __('', 'mms'))->set_width(30)
 			->set_attribute('placeholder', 'tiktok'),
-		Field::make('text', 'youtube', __('', 'mms'))->set_width(50)
+		Field::make('image', 'icon_youtube', __('', 'mms'))->set_width(20),
+		Field::make('text', 'youtube', __('', 'mms'))->set_width(30)
 			->set_attribute('placeholder', 'youtube'),
+		Field::make('image', 'icon_linkedin', __('', 'mms'))->set_width(20),
+		Field::make('text', 'linkedin', __('', 'mms'))->set_width(30)
+			->set_attribute('placeholder', 'linkedin'),
+		Field::make('image', 'icon_twitter', __('', 'mms'))->set_width(20),
+		Field::make('text', 'twitter', __('', 'mms'))->set_width(30)
+			->set_attribute('placeholder', 'twitter'),
 	])
 
 	->add_tab(__('Scripts', 'mms'), [
